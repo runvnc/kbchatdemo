fa39cfe8155a7311b94de69f0284f87835bd09b2
Jason Livesay <runvnc@gmail.com>
2023-03-23 02:22:35 -0500

    update

--- server.mjs
+++ server.mjs
@@ -1,46 +1,30 @@
-import express from 'express';
 import http from 'http';
-import InteractionState from './state.js';
-import { contextualQuery } from './kbqueries.mjs';
+import InteractionState from './state.js'
+import {contextualQuery} from './kbqueries.mjs'
 
-let state;
+let state = await InteractionState.get('testacct')
+await state.clear()
 
-const app = express();
-const server = http.createServer(app);
-
-// Serve static files from the 'static/' directory
-app.use(express.static('static'));
-
-// Handle POST requests to '/query'
-app.get('/query', express.json(), async (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-  res.flushHeaders();
-  console.log(req.query, res)
-  let id = 0;
-  const sendEvent = (data) => {
-    res.write(`id: ${id}\n`);
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-    id++;
-  };
-
-  const query = req.query;
-  console.log(query);
-  if (!query) {
-    res.end({response:'.'})
-    return
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/query') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', async () => {
+      const query = JSON.parse(body).query;
+      console.log(query);
+      let result = await contextualQuery(query, 'testacct', state, (d) => {
+        res.write(d.content)
+      })
+      //res.setHeader('Content-Type', 'application/json');
+      //res.end(JSON.stringify({ response: 'KB PLACEHOLDER RESPONSE' }));
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
-  let result = await contextualQuery(query, 'testacct', state, (d) => {
-    sendEvent({ response: d.content });
-  });
 });
-
-state = await InteractionState.get('testacct');
-await state.clear();
-
-
-server.listen(3600, async () => {
-  console.log('Server running on port 3600');
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
-
---
+++ sssserver.mjs
@@ -0,0 +1,46 @@
+import express from 'express';
+import http from 'http';
+import InteractionState from './state.js';
+import { contextualQuery } from './kbqueries.mjs';
+
+let state;
+
+const app = express();
+const server = http.createServer(app);
+
+// Serve static files from the 'static/' directory
+app.use(express.static('static'));
+
+// Handle POST requests to '/query'
+app.get('/query', express.json(), async (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders();
+  console.log(req.query, res)
+  let id = 0;
+  const sendEvent = (data) => {
+    res.write(`id: ${id}\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+    id++;
+  };
+
+  const query = req.query;
+  console.log(query);
+  if (!query) {
+    res.end({response:'.'})
+    return
+  }
+  let result = await contextualQuery(query, 'testacct', state, (d) => {
+    sendEvent({ response: d.content });
+  });
+});
+
+state = await InteractionState.get('testacct');
+await state.clear();
+
+
+server.listen(3600, async () => {
+  console.log('Server running on port 3600');
+});
+
