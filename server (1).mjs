b1b238404cde4dc969f70c8036e47e9c6e9ff5d6
Jason Livesay <runvnc@gmail.com>
2023-03-23 01:59:07 -0500

    update

--- server (1).mjs
+++ server (1).mjs
@@ -1,22 +1,3 @@
-
-import {InteractionState} from './state.js'
-import {contextualQuery} from './kbqueries.mjs'
-
-let state = await InteractionState.get('testacct')
-await state.clear()
-
-     //res.setHeader('Content-Type', 'application/json');
-      //res.end(JSON.stringify({ response: 'KB PLACEHOLDER RESPONSE' }));
-    });
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
-});
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
-
 import http from 'http';
 const server = http.createServer((req, res) => {
   if (req.method === 'POST' && req.url === '/query') {
@@ -37,10 +18,8 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       const query = JSON.parse(body).query;
       console.log(query);
-      let result = await contextualQuery(query, state, (d) => {
-        sendEvent({ response: d.content });
-      })
- 
+      sendEvent({ response: 'KB PLACEHOLDER RESPONSE' });
+      res.end();
     });
   } else {
     res.statusCode = 404;
---
+++ sse.js
@@ -0,0 +1,15 @@
+const chatBody = document.querySelector('.chat-body');
+const eventSource = new EventSource('/query');
+eventSource.addEventListener('message', (e) => {
+  const data = JSON.parse(e.data);
+  const message = `
+    <div class="message">
+      <p>${data.query}</p>
+    </div>
+    <div class="message">
+      <p>${data.response}</p>
+    </div>
+  `;
+  chatBody.insertAdjacentHTML('beforeend', message);
+  chatBody.scrollTop = chatBody.scrollHeight;
+});
